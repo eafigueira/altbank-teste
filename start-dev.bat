7038fd3d6d1171dd07ea8ec8f0ae1c1e69616090
emerson.figueira <eafigueira@gmail.com>
2025-03-28 23:20:08 -0300

    ok

--- start-dev.bat
+++ start-dev.bat
@@ -3,9 +3,9 @@ echo 🔧 Iniciando Docker Compose...
 docker-compose up -d
 
 if %errorlevel% neq 0 (
-    echo ❌ Erro ao subir o docker-compose.
+    echo Erro ao subir o docker-compose.
     exit /b %errorlevel%
 )
 
-echo 🚀 Iniciando Quarkus em modo dev...
+echo Iniciando Quarkus em modo dev...
 call mvnw quarkus:dev
